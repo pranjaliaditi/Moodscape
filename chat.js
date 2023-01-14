35c3f971766c500e8e2d58c32a97c251513da43e
Girisha Sahdev <104763901+girishatechie@users.noreply.github.com>
2023-01-15 01:33:29 +0530

    Update chat.js

--- chat.js
+++ chat.js
@@ -83,10 +83,7 @@ function buttonSendText(sampleText) {
     $("#chatbox").append(userHtml);
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    //Uncomment this if you want the bot to respond to this buttonSendText event
-    // setTimeout(() => {
-    //     getHardResponse(sampleText);
-    // }, 1000)
+    
 }
 
 function sendButton() {
